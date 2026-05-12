4a9e6297524148d54656666eaab10c5489573f09
Maruf520 <md.maruf5201@gmail.com>
2026-05-12 13:32:05 +1000

    User model created.

---
+++ src/com/genzfits/model/User.java
@@ -0,0 +1,23 @@
+package com.genzfits.model;
+
+public abstract class User {
+
+    protected final String id;
+    protected final String email;
+    protected final String passwordHash;
+    protected final String fullName;
+
+    protected User(String id, String email, String passwordHash, String fullName) {
+        this.id = id;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.fullName = fullName;
+    }
+
+    public String getId()           { return id; }
+    public String getEmail()        { return email; }
+    public String getPasswordHash() { return passwordHash; }
+    public String getFullName()     { return fullName; }
+
+    public abstract String getRoleLabel();
+}
